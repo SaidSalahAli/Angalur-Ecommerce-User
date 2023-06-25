d6568ff0b6d88fd4b35d91301f389846a8f3b2e2
SaidSalahAli <saidsalah375@gmail.com>
2023-06-25 09:22:38 +0300

    first commit

---
+++ src/app/products/models/product.ts
@@ -0,0 +1,14 @@
+
+
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    category: string;
+    description: string;
+    images: string[];
+    rating: number;
+    stock: number;
+    thumbnail: string;
+    discountPercentage: number;
+  }
